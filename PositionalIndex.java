58e702db613f2772b6543aa2240704d2380d666d
Max <mrohrer@iastate.edu>
2023-11-17 13:43:03 -0600

    Added initial methods and classes

---
+++ PositionalIndex.java
@@ -0,0 +1,47 @@
+public class PositionalIndex {
+
+    public String folder;
+
+    public PositionalIndex(String folder) {
+        this.folder = folder;
+    }
+
+    public int termFrequency(String term, String doc) {
+
+
+        return 0;
+    }
+
+    public int docFrequency(String term) {
+
+
+
+        return 0;
+    }
+
+    public String postingsList(String t) {
+
+        return null;
+    }
+
+    public double weight(String t, String d) {
+
+        return 0.0;
+    }
+
+    public double TPScore(String query, String doc) {
+
+        return 0.0;
+    }
+
+    public double VSScore(String query, String doc) {
+
+        return 0.0;
+    }
+
+    public double Relevance(String query, String doc) {
+
+        return 0.0;
+    }
+
+}
---
+++ QueryProcessor.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class QueryProcessor {
+
+    public QueryProcessor() {
+
+    }
+
+    public ArrayList<String> topKDocs(String query, int k){
+
+        return null;
+    }
+
+}
