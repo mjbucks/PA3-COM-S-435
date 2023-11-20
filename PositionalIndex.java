acb498155ab6295f3e949779f04c99e012e00962
Max <mrohrer@iastate.edu>
2023-11-19 19:19:25 -0600

    Term and doc frequency

--- PositionalIndex.java
+++ PositionalIndex.java
@@ -1,22 +1,46 @@
+import java.io.*;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class PositionalIndex {
 
     public String folder;
+    String[] unprocessedDocs;
+    ArrayList<ArrayList<String>> docs;
 
-    public PositionalIndex(String folder) {
+
+    public PositionalIndex(String folder) throws FileNotFoundException {
         this.folder = folder;
+        this.unprocessedDocs = allDocs();
+        File file;
+        for (int doc = 0; doc < unprocessedDocs.length; doc++) {
+            file = new File(folder + unprocessedDocs[doc]);
+            docs.add(preProcess(file));
+        }
+
     }
 
     public int termFrequency(String term, String doc) {
+        int index = getIndexOfDoc(doc);
 
-
-        return 0;
+        return Collections.frequency(docs.get(index), term);
     }
 
     public int docFrequency(String term) {
+        int numTimes = 0;
 
-
-
-        return 0;
+        for (int i = 0; i < docs.size(); i++){
+            if(docs.get(i).contains(term)) {
+                numTimes++;
+            }
+        }
+        return numTimes;
     }
 
     public String postingsList(String t) {
@@ -42,6 +66,52 @@ public class PositionalIndex {
     public double Relevance(String query, String doc) {
 
         return 0.0;
+    }
+
+    public ArrayList<String> preProcess(File file) throws FileNotFoundException {
+        ArrayList<String> document = new ArrayList<String>();
+
+        Scanner scan = new Scanner(new BufferedReader(new FileReader(file)));
+
+        while (scan.hasNext()){
+            String word = scan.next();
+            word = word.toLowerCase();
+            word = word.replaceAll("[,\\[\\]\"\'{}:;()\\?]", "");
+            if (!word.equals("are") && !word.equals("the") && !word.equals("is")){
+                document.add(word);
+            }
+        }
+        return document;
+    }
+
+    public String[] allDocs() {
+        try {
+            Path folderPath = Paths.get(folder);
+            long count = Files.list(folderPath).count();
+            String[] allDocs = new String[(int) count];
+            int i = 0;
+            try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(folderPath)) {
+                for (Path path : directoryStream) {
+                    if (Files.isRegularFile(path)) {
+                        allDocs[i] = String.valueOf(path.getFileName());
+                        i++;
+                    }
+                }
+            }
+            return allDocs;
+        } catch (IOException e) {
+            System.err.println("An I/O error occurred: " + e.getMessage());
+        }
+        return null;
+    }
+
+    public int getIndexOfDoc(String doc) {
+        for (int i = 0; i < unprocessedDocs.length; i++) {
+            if (unprocessedDocs[i] == doc) {
+                return i;
+            }
+        }
+        return -1;
     }
 
 }
